c94f03768f5b0f71997e0ff8d1b4daed33aa1046
carlosvelascosala27-sys <carlos.velascosala27@gmail.com>
2026-05-20 13:05:52 +0200

    Cambio de redirección de login a principal.php

--- Login/login.php
+++ Login/login.php
@@ -41,7 +41,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             if ($usuario['rol'] === 'admin') {
                 header("Location: ../Admin/admin.php");
             } else {
-                header("Location: ../Principal/principal.html");
+                header("Location: ../Principal/principal.php");
             }
             exit(); // Importante: detiene el PHP después de redirigir
 
